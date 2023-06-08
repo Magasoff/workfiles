1828ddc9da1b77ef675952923b5c57b8a18c1a05
Magasoff <117380561+Magasoff@users.noreply.github.com>
2023-06-08 09:57:12 +0500

    1

---
+++ src/main/java/com/example/workfiles/WorkfilesApplication.java
@@ -0,0 +1,13 @@
+package com.example.workfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkfilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkfilesApplication.class, args);
+    }
+
+}
